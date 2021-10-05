b524ab22646cd3eeb65c1b73b2ba312baea3cdd8
kozakovk <kozakov.k@brights.cloud>
2021-10-05 16:25:36 +0300

    init

---
+++ MConsole/MConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
